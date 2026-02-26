0efd20df52e31e0f1bd43884eb3e6bee0fb6a7f2
Charlie-Ash <charlieash245@gmail.com>
2026-02-27 01:14:47 +0800

    Added a few other tweaks in Vector_Storing.py

--- RAG_Project/src/Vector_Storing.py
+++ RAG_Project/src/Vector_Storing.py
@@ -1,10 +1,12 @@
 import chromadb
+from chromadb.config import Settings
 
 # To store: vectors + text + metadata
 class VectorStorage:
     
     def __init__(self):
 
-        chromaDB_client = chromadb.Client()
-        collection = chromaDB_client.create_collection("TheCollection")
+        chromaDB_client = chromadb.Client(Settings(persist_directory="./chroma_db"))  # Where the data base is, will still exist in this file after code executes
+        chromaDB_client.delete_collection("the_collection")  # Deletes the collection created from the previous run (in ./chroma_db)
+        collection = chromaDB_client.create_collection("the_collection")
         
